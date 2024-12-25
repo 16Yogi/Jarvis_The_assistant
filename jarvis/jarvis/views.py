ec071146091ae23c5ab719dcbf06a530b8b3554f
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-12-26 00:17:05 +0530

    Commit
    Commit

--- jarvis/jarvis/views.py
+++ jarvis/jarvis/views.py
@@ -1,9 +1,32 @@
+from apiip import apiip
+from pprint import pprint
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    output = myfun(request)
-    return render(request,'index.html',{'output':output})
 
-def myfun(request):
-    return "Hello"
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import time
+# from playsound import playsound
+# import openai
+import pyjokes
+import requests
+from bs4 import BeautifulSoup
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def home(request):
+    if request.method == "POST":
+        data = request.POST.get("speech","")
+        print("this is data", data)
+        if "hello" in data:
+         return render(request, 'index.html', {'output':"hii"})
+    return render(request, 'index.html')
