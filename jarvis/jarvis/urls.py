4ae03f187cb7e890206b7709eb4e344e930413a0
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-12-28 14:02:03 +0530

    Done
    All most done

--- jarvis/jarvis/urls.py
+++ jarvis/jarvis/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from . import views
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     # path('',views.home,name='home'),
     path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jarvis/jarvis/views.py
+++ jarvis/jarvis/views.py
@@ -8,6 +8,12 @@ from apiip import apiip
 import requests
 import wikipedia
 import webbrowser
+from django.templatetags.static import static
+from django.conf import settings
+
+
+video = None
+
 
 @csrf_exempt
 def home(request):
@@ -15,8 +21,10 @@ def home(request):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
+    global video
 
     # Wish function
+
     def wishMe():
         hour = int(datetime.datetime.now().hour)
         if 0 <= hour < 12:
@@ -25,7 +33,6 @@ def home(request):
             return "Good Afternoon!"
         else:
             return "Good Evening!"
-
     if request.method == "POST":
         # Get speech data from POST request
         data = request.POST.get("speech", "").lower()
@@ -50,6 +57,7 @@ def home(request):
             engine.runAndWait()
         elif "i love you" in data:
             response = "I love you too! You're amazing!"
+            data = settings.MEDIA_URL + 'kiss.mp4'
             engine.say(response)
             engine.runAndWait()
         elif "dance" in data:
@@ -59,18 +67,21 @@ def home(request):
         elif "tell me a joke" in data:
             joke = pyjokes.get_joke()
             response = f"Yeah! {joke}"
-            engine.say(response,joke)
+            engine.say(response, joke)
             engine.runAndWait()
         elif "shall we go for a date" in data:
             response = "I'm flattered, but I'm just a chatbot. I don't go on dates."
             engine.say(response)
+
             engine.runAndWait()
         elif "i need you" in data:
             response = "Yes, I am always with you."
+            data = settings.MEDIA_URL + 'hug.mp3'
             engine.say(response)
             engine.runAndWait()
         elif "i hate you" in data:
             response = "I think I'm not your type, and that's okay. "
+            data = settings.MEDIA_URL + 'hate.mp4'
             engine.say(response)
             engine.runAndWait()
 
@@ -81,17 +92,18 @@ def home(request):
 
         elif "kiss you" in data:
             response = "Still working........."
+            data = settings.MEDIA_URL + 'kiss.mp4'
             engine.say(response)
             engine.runAndWait()
-            
+
         elif "i am suffering from fever" in data:
             response = "Oh no! I'm sorry to hear that. Please make sure to eat some food, take your medicine, and get plenty of rest. Take care of yourself. Would you like me to call a doctor or your mom?"
             engine.say(response)
-            engine.runAndWait()            
+            engine.runAndWait()
         elif "you are a stupit" in data:
             response = "I'm sorry to hear that. Let's focus on something positive."
             engine.say(response)
-            engine.runAndWait()        
+            engine.runAndWait()
         elif "good morning" in data:
             response = "Hello, good morning, sir! Have a nice day. I am Jarvis. How can I help you?"
             engine.say(response)
@@ -124,7 +136,7 @@ def home(request):
             current_time = datetime.datetime.now().strftime("%H:%M:%S")
             response = "The current time is."
             response = current_time
-            engine.say(response,current_time)
+            engine.say(response, current_time)
             engine.runAndWait()
         elif "your name" in data:
             response = "I am Jarvis, your personal assistant."
@@ -145,42 +157,15 @@ def home(request):
         elif "play music" in data:
             response = "I can't play music directly, but I can suggest some great songs for you!"
             engine.say(response)
+            data = settings.MEDIA_URL + 'hate.mp4'
             engine.runAndWait()
         elif "tell me about yourself" in data:
             response = "I am Jarvis, your personal assistant. I am here to help you with your tasks and make your day easier!"
             engine.say(response)
             engine.runAndWait()
-        # news
-        # elif 'today news' in data:
-        #     api_key = "bb31751c01cf4b3bbd6709d79ee51d38"
-        #     url = 'https://newsapi.org/v2/everything'
-        #     params = {'q': 'Python', 'sortBy': 'popularity', 'apiKey': api_key}
-        #     response = requests.get(url, params=params)
-            
-        #     if response.status_code == 200:
-        #         articles = response.json().get('articles', [])
-        #         news_titles = [article.get('title') for article in articles if article.get('title')]
-        #         response_message = "News articles: " + ", ".join(news_titles) if news_titles else "No news found."
-        #         engine.say(response_message)
-        #         engine.runAndWait()
-        #     else:
-        #         response_message = f"Failed to fetch news. Error code: {response.status_code}"
-        #         engine.say(response_message)
-        #         engine.runAndWait()
-
-        
-        # #location
-        # elif 'location' or 'where i am' in data:
-        #     def location():
-        #         api_key =  apiip('d85412f9-d2b1-428c-93e6-d14c227e43d4')
-        #         info = api_key.get_location()
-        #         response = f"location:{info}"
-        #         engine.say(response)
-        #         engine.runAndWait()
-        #         location()
-
         elif "wikipedia" in data:
-            data = data.replace("wikipedia", "").strip()  # Remove the "wikipedia" part and any extra spaces
+            # Remove the "wikipedia" part and any extra spaces
+            data = data.replace("wikipedia", "").strip()
             try:
                 # Get summary of the search term (limit to 2 sentences)
                 response = wikipedia.summary(data, sentences=2)
@@ -188,45 +173,42 @@ def home(request):
                 engine.runAndWait()
             except wikipedia.exceptions.DisambiguationError as e:
                 # Handle case where multiple pages match the query
-                engine.say(f"Multiple results found. Please be more specific. Here are some options: {e.options[:3]}")  # Show up to 3 options
+                # Show up to 3 options
+                engine.say(
+                    f"Multiple results found. Please be more specific. Here are some options: {e.options[:3]}")
                 engine.runAndWait()
             except wikipedia.exceptions.HTTPTimeoutError:
                 # Handle case where Wikipedia is unreachable
-                engine.say("Sorry, there was a problem fetching the information. Please try again later.")
+                engine.say(
+                    "Sorry, there was a problem fetching the information. Please try again later.")
                 engine.runAndWait()
             except wikipedia.exceptions.PageError:
                 # Handle case where no page was found for the query
-                engine.say("Sorry, I couldn't find any information on that topic.")
+                engine.say(
+                    "Sorry, I couldn't find any information on that topic.")
                 engine.runAndWait()
             except Exception as e:
                 # Catch any other errors
                 engine.say(f"An error occurred: {str(e)}")
                 engine.runAndWait()
-        # open websites
-        # elif "open google" in data:
-            # data = data.replace("google", "").strip()
-            # webbrowser.open("google.com")
-        # elif f"open {data}" in data:
-        #     data = data.replace(data, "").strip()
-        #     webbrowser.open(f"{data}.com")
         elif "open" in data:
             website = data.replace("open", "").strip()
             if website:
                 if '.' not in website:
                     extensions = [
-                            '.com', '.org', '.net', '.info', '.biz', '.edu', '.gov', '.int', '.mil', 
-                            '.in', '.us', '.uk', '.ca', '.au', '.de', '.fr', '.jp', '.cn', '.br', 
-                            '.ru', '.sa', '.za', '.ai', '.io', '.tv', '.me', '.app', '.tech', 
-                            '.xyz', '.co', '.store', '.online', '.shop', '.website', '.space', 
-                            '.name', '.pro', '.mobi', '.asia', '.tel', '.museum', '.coop', '.aero'
-                        ]
+                        '.com', '.org', '.net', '.info', '.biz', '.edu', '.gov', '.int', '.mil',
+                        '.in', '.us', '.uk', '.ca', '.au', '.de', '.fr', '.jp', '.cn', '.br',
+                        '.ru', '.sa', '.za', '.ai', '.io', '.tv', '.me', '.app', '.tech',
+                        '.xyz', '.co', '.store', '.online', '.shop', '.website', '.space',
+                        '.name', '.pro', '.mobi', '.asia', '.tel', '.museum', '.coop', '.aero'
+                    ]
                     for ext in extensions:
                         url = f"http://{website}{ext}"
                         try:
                             webbrowser.open(url)
-                            break  
+                            break
                         except:
-                            continue  
+                            continue
                 else:
                     webbrowser.open(f"http://{website}")
             else:
@@ -235,16 +217,13 @@ def home(request):
         else:
             response = f"You said: {data}"
 
-        # Speak the response
-        # engine.say(response)
-        # engine.runAndWait()
+         # Correctly generate the URL for the video
+        video_url = data
+        return JsonResponse({"output": response, "video": video_url})
 
-        # Return response as JSON
-        return JsonResponse({"output": response})
-
-    # Render the HTML page and greet the user
     greeting = wishMe()
+    print("This is video", video)
     engine.say(f"Hello, I am Jarvis. {greeting} How can I help you?")
     engine.runAndWait()
 
-    return render(request, 'index.html', {"output": ""})
+    return render(request, 'index.html')
